0b07d7e26e0517a0fae001142f36eace4fac81d1
Christian Sheen <csheen1725@gmail.com>
2023-04-30 12:12:28 -0400

    create categoryItem component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoryItem from "./components/category-item/category-item.component.jsx";
+import { categories } from "./models/categories.js";
+import "./scss/categories.styles.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="categories-container">
+			{categories.map((category) => (
+				<CategoryItem key={category.id} category={category} />
+			))}
+		</div>
+	);
+};
 
 export default App;
